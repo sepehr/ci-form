d04bd3c562bb8d661891edbc0e1290be27a1e367
Sepehr Lajevardi <me@sepehr.ws>
2012-07-01 14:05:12 +0430

    Form helper placeholder.

---
+++ helpers/MY_form_helper.php
@@ -0,0 +1,15 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter Drupal-inspited Form API plus Bootstrap integration
+ *
+ * @package		CodeIgniter
+ * @author		Sepehr Lajevardi <me@sepehr.ws>
+ * @copyright	Copyright (c) 2012 Sepehr Lajevardi.
+ * @license		http://codeigniter.com/user_guide/license.html
+ * @link		https://github.com/sepehr/ci-form
+ * @version 	Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
